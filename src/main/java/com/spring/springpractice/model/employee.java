da845bf7dd5efa9ca3ec654afd3c7dc12c167a9e
ChaharAbhishek <abhisheklay123@gmail.com>
2021-06-29 15:04:12 +0530

    passing data dynamically using postman

--- src/main/java/com/spring/springpractice/model/employee.java
+++ src/main/java/com/spring/springpractice/model/employee.java
@@ -1,21 +1,26 @@
 package com.spring.springpractice.model;
 
+
 public class employee {
+
     String name;
     int age;
     String dept;
 
+
     //in this first we create constructor then getter & setter .
     //parameterised constructor (at the time of object creation we assign a value).
-
     public employee(String name, int age, String dept)
     {
         this.name = name;
         this.age = age;
         this.dept = dept;
     }
+
+
     //getter is used for getting the details
-    //setter is used for change the details\
+    //setter is used for change the details
+
     public String getName() {
         return name;
     }
